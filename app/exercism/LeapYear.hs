4011e05b5d73f5e29eb6482b7dbd3d71ec2e94a3
Johannes Lenk <Lenk.Johannes@gmx.de>
2022-12-08 23:42:37 +0100

    exercism 2

--- app/exercism/LeapYear.hs
+++ app/exercism/LeapYear.hs
@@ -2,8 +2,8 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year
-  if mod year 400 == 0 = True
-  else if mod year 100 == 0 = False
-  else if mod year 4 == 0 = True
-  else otherwise = False
+  | mod year 400 == 0 = True
+  | mod year 100 == 0 = False
+  | mod year 4 == 0 = True
+  | otherwise = False
 
